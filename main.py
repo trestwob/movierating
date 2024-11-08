ee53a5c9ef5508624859c0ff358291c465012283
trestwob <nilanjanmridha89@gmail.com>
2024-11-08 00:41:42 -0800

    Datasets Merged

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import pandas as pd
-coffee = pd.read_csv('ml-latest-small/movies.csv')
-print(coffee)
+movies = pd.read_csv('ml-latest-small/movies.csv', delimiter=',', header=None, names=['movieId', 'title', 'genres']).drop(0)
+ratings = pd.read_csv('ml-latest-small/ratings.csv', delimiter=',', header=None, names=['userId', 'movieId', 'rating', 'timestamp']).drop(0)
+
+movie_data = pd.merge(ratings, movies[['movieId', 'title']])
